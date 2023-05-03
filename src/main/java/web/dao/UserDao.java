f8ddea390fa143bba56e0ec8d9e9cbb41dc6437c
Andrey Nichipor <nichipor1990@gmail.com>
2023-05-03 20:47:52 +0600

    Task7fixFormat

--- src/main/java/web/dao/UserDao.java
+++ src/main/java/web/dao/UserDao.java
@@ -6,9 +6,13 @@ import java.util.List;
 
 public interface UserDao {
     void createUser(User user);
+
     User getUser(long id);
+
     List<User> getAllUsers();
+
     void updateUser(User user, long id);
+
     void deleteUser(long id);
 
 }
--- src/main/java/web/service/UserService.java
+++ src/main/java/web/service/UserService.java
@@ -6,8 +6,12 @@ import java.util.List;
 
 public interface UserService {
     void createUser(User user);
+
     User getUser(long id);
+
     List<User> getAllUsers();
+
     void updateUser(User user, long id);
+
     void deleteUser(long id);
 }
